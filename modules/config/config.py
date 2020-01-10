6841a4ff62b93697d43a7b5461028ade484d5fee
AndyBazualdoQ <andy.bazualdo@jalasoftdevelopment.onmicrosoft.com>
2020-01-10 11:07:49 -0800

    return previous config

--- modules/config/config.py
+++ modules/config/config.py
@@ -1,7 +1,6 @@
 # bnk
 class Config():
-    #DRIVER_TYPE= "ChromeHeadless"
-    DRIVER_TYPE= "Chrome"
+    DRIVER_TYPE= "ChromeHeadless"
     EXECUTABLE_PATH="C:/D/PyWorkspace/amazon/drivers/chromedriver.exe"
     # BASE_URL = "http://blazedemo.com/"
     BASE_URL = "http://www.office.com/"
