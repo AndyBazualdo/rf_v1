a37a9f31a39576fe8bec05220ea183a70ff8c66a
AndyBazualdoQ <andy.bazualdo@jalasoftdevelopment.onmicrosoft.com>
2020-01-21 13:11:24 -0800

    q 5

--- startViewers.py
+++ startViewers.py
@@ -1,8 +1,8 @@
 import subprocess
 
-command1 = subprocess.Popen(['vncviewer', '192.168.196.134:5900'])
-command2 = subprocess.Popen(['vncviewer', '192.168.196.134:5901'])
+command1 = subprocess.Popen(['vncviewer', 'localhost:5900'])
+command2 = subprocess.Popen(['vncviewer', 'localhost:5901'])
 
 
-print command1
-print command2
+#print command1
+#print command2
